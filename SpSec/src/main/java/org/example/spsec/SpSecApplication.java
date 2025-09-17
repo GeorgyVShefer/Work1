ad932cbedb8a9a216fae29bdbac531a66f760e77
GeorgyVShefer <156106194+GeorgyVShefer@users.noreply.github.com>
2025-09-17 16:40:07 +0300

    commit1

---
+++ SpSec/src/main/java/org/example/spsec/SpSecApplication.java
@@ -0,0 +1,14 @@
+package org.example.spsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpSecApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpSecApplication.class, args);
+    }
+
+}
---
+++ gen/docs/UserDTO.md
@@ -0,0 +1,15 @@
+
+
+# UserDTO
+
+## Properties
+
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**id** | **Long** |  |  [optional]
+**username** | **String** |  |  [optional]
+**email** | **String** |  |  [optional]
+**password** | **String** |  |  [optional]
+
+
+
